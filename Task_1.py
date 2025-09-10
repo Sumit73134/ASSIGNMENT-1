b8885ee4fc8f02d507d3bd5480e74070197f1819
Sumit Kumar <sumitshrivastav416@gmail.com>
2025-09-10 16:18:39 +0530

    In Task_1 mathematical operation And In Task_2 welcome message

---
+++ Task_1.py
@@ -0,0 +1,14 @@
+a= int(input("a: "))
+b= int(input("b: "))
+
+#Addition
+print("Addition:", a+b)
+
+#subtraction
+print("subtraction:", a-b)
+
+# Multiplication
+print("Multiplication:", a*b)
+
+# Division
+print("Division:",a/b)
